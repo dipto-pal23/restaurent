9eb34714878a38bdf03dc3878e67bdd874a4318b
dipto-pal23 <dipto4050@gmail.com>
2023-11-18 15:28:28 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,41 @@
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">about</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#offer">offer</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#menu">menu</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#service">service</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#Gallery">Gallery</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#Reservation">Reservation</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">contcet</a>
+                        </li>
+
+                    </ul>
+
+                </form>
+            </div>
+        </div>
+    </body>
